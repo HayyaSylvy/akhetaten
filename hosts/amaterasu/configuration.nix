793e97dcbf482f9bd5494a392eead1242f756311
HayyaSylvy <hannahmarieribeiro@proton.me>
2025-12-02 17:00:37 -0300

    Adicionando minhas configurações para o DankMaterialShell

--- hosts/amaterasu/configuration.nix
+++ hosts/amaterasu/configuration.nix
@@ -99,7 +99,24 @@
 
   stylix = {
      enable = true;
-     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
+     base16Scheme = {
+    	base00 = "282828";
+    	base01 = "3c3836";
+    	base02 = "504945";
+    	base03 = "665c54";
+    	base04 = "bdae93";
+    	base05 = "d5c4a1";
+    	base06 = "ebdbb2";
+    	base07 = "fbf1c7";
+    	base0D = "fb4934"; # Red
+    	base09 = "fe8019";
+    	base0A = "fabd2f";
+    	base0B = "b8bb26";
+    	base0C = "8ec07c";
+    	base08 = "83a598"; #Blue
+    	base0E = "d3869b";
+	base0F = "d65d0e";
+     };
      image = ./gruvy.png;
      enableReleaseChecks = false;
   };
--- hosts/amaterasu/home.nix
+++ hosts/amaterasu/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -20,10 +20,89 @@
      fastfetch
   ];
 
+    xdg.configFile."GruvboxRose.json".text = ''
+    {
+    "dark": {
+    	"name": "Gruvbox-Rose Dark",
+    	"primary": "#fb4934",
+    	"primaryText": "#1b1b1b",
+    	"primaryContainer": "#98971A",
+    	"secondary": "#98971A",
+    	"surface": "#1b1b1b",
+    	"surfaceText": "#FBF1C7",
+    	"surfaceVariant": "#282828",
+    	"surfaceVariantText": "#928374",
+    	"surfaceTint": "#98971A",
+    	"background": "#1b1b1b",
+    	"backgroundText": "#FBF1C7",
+    	"outline": "#928374",
+    	"surfaceContainer": "#282828",
+    	"surfaceContainerHigh": "#3C3836",
+    	"surfaceContainerHighest": "#504945",
+    	"error": "#FB4934",
+    	"warning": "#FABD2F",
+    	"info": "#83A598",
+    	"matugen_type": "scheme-expressive"
+  	},
+    "light": {
+    	"name": "Gruvbox-Rose Light",
+    	"primary": "#EA6962",
+    	"primaryText": "#FBF1C7",
+    	"primaryContainer": "#98971A",
+    	"secondary": "#98971A",
+    	"surface": "#FBF1C7",
+    	"surfaceText": "#1b1b1b",
+    	"surfaceVariant": "#EBDBB2",
+    	"surfaceVariantText": "#928374",
+    	"surfaceTint": "#98971A",
+    	"background": "#FBF1C7",
+    	"backgroundText": "#1b1b1b",
+    	"outline": "#928374",
+    	"surfaceContainer": "#EBDBB2",
+    	"surfaceContainerHigh": "#D5C4A1",
+    	"surfaceContainerHighest": "#BDAE93",
+    	"error": "#CC241D",
+    	"warning": "#D79921",
+    	"info": "#458588",
+    	"matugen_type": "scheme-expressive"
+  	}
+       }
+    '';
+
+
   programs.nixvim.enable = true;
   services.wpaperd.enable = true;
   programs.dankMaterialShell = {
    enable = true;
+   default.settings = {
+        currentThemeName = "custom";
+	customThemeFile = "/home/ladyhayya/.config/GruvboxRose.json";
+	screenPreferences = { 
+		"wallpaper" = "[]";        
+        };
+	dockTransparency = 0.9;
+	blurWallpaperOnOverview = true;
+	weatherLocation = "João Pessoa, Região Nordeste";
+	weatherCoordinates = "-7.1215981,-34.8820280";
+	launcherLogoMode = "os";
+	showDock = true;
+	dockGroupByApp = true;
+	dockOpenOnOverview = true;
+	dockAutoHide = true;
+	dockIndicatorStyle = "line";
+	dockBorderEnabled = true;
+	dockBorderColor = "primary";
+	powerActionConfirm = false;
+	dockPosition = 1;
+	dockSpacing = 10;
+	dockBottomGap = -20;
+	dockMargin = 0;
+	dockIconSize = 50;
+	dockBorderOpacity = 1;
+	dockBorderThickness = 2;
+	qtThemingEnabled = false;
+	gtkThemingEnabled = false;
+   };
     niri = {
       enableSpawn = true;      # Auto-start DMS with niri
     };
